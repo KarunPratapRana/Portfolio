58424253862511d62bec8780a4f39be7dacac5ca
Karun Pratap Rana <ksingh@karundev.ssqueal.org>
2018-01-16 09:05:50 +0530

    jsonld seo changes

---
+++ public/portfolio/js/jsonldSchema.js
@@ -0,0 +1,32 @@
+  {
+  "@context": "http://schema.org",
+  "@type": "Person",
+  "address": {
+  "@type": "PostalAddress",
+  "addressLocality": "G-179 sector 22",
+  "addressRegion": "Noida",
+  "postalCode": "201301",
+  "streetAddress": "Near Mother Dairy and Shiv Mandir Sector 22"
+  },
+  "email": "ajaykumarpandeya@gmail.com",
+  "image": "https://ajay-financialadvisor.herokuapp.com/portfolio/images/man.jpg",
+  "jobTitle": "Finance & Accountant",
+  "name": "Ajay Kumar Pandey",
+  "gender": "male",
+  "nationality": "Indian",
+  "telephone": "9643862867",
+  "url": "https://ajay-financialadvisor.herokuapp.com/",
+  "sameAs" : [ "https://www.facebook.com/",
+  "https://www.linkedin.com/",
+  "http://twitter.com/",
+  "http://instagram.com/",
+  "https://plus.google.com/"]
+  }
+  window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};
+  var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");
+  a.type="application/ld+json",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";
+  var n=document.getElementsByTagName("script")[0];
+  n.parentNode.insertBefore(a,n);
+  for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+  heap.load("3440292096");
+
