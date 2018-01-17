23c2b08c02960d0b750fbabd3ffeb3edc1e7b3fc
Karun Pratap Rana <ksingh@karundev.ssqueal.org>
2018-01-17 23:52:17 +0530

    fixes to improve seo performance

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var controllers = require('./controllers');
 var path = require('path');
 var bodyParser = require('body-parser');
+var etag = require('etag')
 
 var app = express();
 // parse application/x-www-form-urlencoded
@@ -11,7 +12,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static('public', { maxAge: 24*3600000 }))
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+    res.set('Expires', Thu, 15 Apr 2018 20:00:00 GMT);
+    res.set('Content-Encoding', gzip);
+    res.set('ETag', etag(body))
+  }
+}
+app.use(express.static(__dirname + 'public', options));
 
 app.get('/', function (req, res) {
   console.log("Welcome to Ajay's Portfolio");
