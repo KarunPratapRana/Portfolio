178112919416af7619d28791d97bf12837c4f4ef
Karun Pratap Rana <ksingh@karundev.ssqueal.org>
2018-01-31 22:54:17 +0530

    mailer fix

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var compression = require('compression')
 var controllers = require('./controllers');
 var path = require('path');
 var bodyParser = require('body-parser');
-var cache = require('./middleware/cacheMiddleware');
+var cache = require('./middleware/cacheMiddleware'),
+    passport = require( 'passport' );
 
 var app = express();
 app.use(compression())
@@ -36,6 +37,24 @@ app.get('/', cache(2), function (req, res) {
 })
 app.post('/contact_me', controllers.user.post);
 app.get('/contact_me', cache(2), controllers.user.get);
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google', passport.authenticate('google', { scope: ['https://mail.google.com/'] }));
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get( '/auth/google/callback', 
+      passport.authenticate( 'google', { 
+        successRedirect: '/',
+        failureRedirect: '/login'
+}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, function(){
--- services/mail.js
+++ services/mail.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 var pug = require('pug');
 var redis = require("redis"),
+    passport = require( 'passport' );
     redisClient = require('./redisConfig');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var redisPort = (process.env.NODE_ENV ==='production')?19628 : 6379,
     redisHost = (process.env.NODE_ENV ==='production')?'redis-19628.c13.us-east-1-3.ec2.cloud.redislabs.com' : '127.0.0.1';
 
@@ -17,30 +19,49 @@ var kue = require('kue'),
 var mail = function(argument) {
 	
 	function email(data, done) {
-	  nodemailer.createTestAccount((err, account) => {		
-		    let transporter = nodemailer.createTransport({
-		        service: 'Gmail',
-		        auth: {
-		            user: 'ajaybani05@gmail.com', // generated ethereal user
-		            pass: 'Ajay@143*' // generated ethereal password
-		        }
-		    });	
+		//console.log(data);
+	    passport.use(new GoogleStrategy({
+	        clientID: "114697355005-3fbioarctn450cd9a0g6ff3a9clrb087.apps.googleusercontent.com",
+	        clientSecret: "6VZxjEKHZb_IdrNHI7dIG-LQ",
+	        callbackURL: "https://ajay-financialadvisor.herokuapp.com/auth/google/callback"
+	      },
+	      function(accessToken, refreshToken, profile, cb) {
+	      		console.log(accessToken +refreshToken+ profile);
+	      		//process.nextTick(function () {
+		      		nodemailer.createTestAccount((err, account) => {		
+					    let transporter = nodemailer.createTransport({
+					        service: 'Gmail',
+					        XOAuth2: {
+						      user: "ajaybani05@gmail.com",
+						      clientId: "114697355005-3fbioarctn450cd9a0g6ff3a9clrb087.apps.googleusercontent.com",
+						      clientSecret: "6VZxjEKHZb_IdrNHI7dIG-LQ",
+						      refreshToken:refreshToken,
+						      accessToken:accessToken,
+						      timeout: 3600
+						    }
+					    });				
 
-		    let mailOptions = {
-		        from: 'Ajay Portfolio 👥 <ajaybani05@gmail.com>', // sender address
-		        to: data.to, // list of receivers
-		        subject: 'Comment on your profile', // Subject line
-		        html: data.html
-		    };		
+					    let mailOptions = {
+					        from: 'Ajay Portfolio 👥 <ajaybani05@gmail.com>', // sender address
+					        to: data.to, // list of receivers
+					        subject: 'Comment on your profile', // Subject line
+					        html: data.html
+					    };					
 
-		    // send mail with defined transport object
-		    transporter.sendMail(mailOptions, (error, info) => {
-		        if (error) {
-		            return console.log(error);
-		        }	
-		    });
-		});
-	  done();
+					    // send mail with defined transport object
+					    transporter.sendMail(mailOptions, (error, info) => {
+					        if (error) {
+					            return console.log(error);
+					        }	
+					    });
+					});
+					console.log(profile);
+					done();
+					//return done(null, profile);
+				//});
+	    	}
+
+	    ));
 	}
 
 	function send(formData) {
@@ -49,7 +70,7 @@ var mail = function(argument) {
 		  to: 'ajaykumarpandeya@gmail.com',
 		  html: htmlTemplate
 		}).save( function(err){
-		   if( !err ) console.log('mail successfully sent to ajay');
+		   if( !err ) console.log('sending mail to ajay');
 		});
 
 		queue.process('email', function(job, done){
